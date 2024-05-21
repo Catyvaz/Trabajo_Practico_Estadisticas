b6ae06b828ca78665d6917a8811b967c080b08ed
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-05-21 12:45:47 -0300

    modificando el input

--- #Trabajo practico evaluativo Estadistica.py
+++ #Trabajo practico evaluativo Estadistica.py
@@ -1,92 +1,99 @@
-def MEDIA(lista):
-    media = sum(lista)/len(lista)
-    return media 
+# def MEDIA(lista):
+#     media = sum(lista)/len(lista)
+#     return media 
 
-def CALCULAR_MODA(lista):
-    frequencia= {}
-    for elemento in lista:
-        if elemento in frequencia:
-            frequencia[elemento] +=1
-        else:
-            frequencia[elemento] = 1
-    moda = None
-    maxFrequencia= 0
-    for elemento, frequencia in frequencia.items():
-        if frequencia>maxFrequencia:
-            moda=elemento
-            maxFrequencia=frequencia
+# def CALCULAR_MODA(lista):
+#     frequencia= {}
+#     for elemento in lista:
+#         if elemento in frequencia:
+#             frequencia[elemento] +=1
+#         else:
+#             frequencia[elemento] = 1
+#     moda = None
+#     maxFrequencia= 0
+#     for elemento, frequencia in frequencia.items():
+#         if frequencia>maxFrequencia:
+#             moda=elemento
+#             maxFrequencia=frequencia
               
-    return moda
+#     return moda
 
-# calcular promedio(sumatoria de todos los datos / cantidad de datos)
-def CALCULAR_PROMEDIO(lista):
-    if len(lista) == 0:
-        return 0
-    return sum(lista) / len(lista)
+# # calcular promedio(sumatoria de todos los datos / cantidad de datos)
+# def CALCULAR_PROMEDIO(lista):
+#     if len(lista) == 0:
+#         return 0
+#     return sum(lista) / len(lista)
 
-# calcular desviacion estandar((sumatoria de todos los datos - prom)**2/cant de datos -1)
-def DESVIACION_ESTANDAR(lista):
-    n = len(lista)
-    if n <= 1:
-        return 0
-    promedio = CALCULAR_PROMEDIO(lista)
-    suma_resta_cuadrado = (sum(lista) - promedio) ** 2
-    desviacion = (suma_resta_cuadrado / (n -1)) ** 0.5
-    return desviacion 
+# # calcular desviacion estandar((sumatoria de todos los datos - prom)**2/cant de datos -1)
+# def DESVIACION_ESTANDAR(lista):
+#     n = len(lista)
+#     if n <= 1:
+#         return 0
+#     promedio = CALCULAR_PROMEDIO(lista)
+#     suma_resta_cuadrado = (sum(lista) - promedio) ** 2
+#     desviacion = (suma_resta_cuadrado / (n -1)) ** 0.5
+#     return desviacion 
 
-def CALCULAR_MEDIANA(lista):
-    listaOrdenada=sorted(lista)
-    longitudLista=len(listaOrdenada)
+# def CALCULAR_MEDIANA(lista):
+#     listaOrdenada=sorted(lista)
+#     longitudLista=len(listaOrdenada)
     
-    if longitudLista % 2 == 0:
-        medioIzq = listaOrdenada[longitudLista // 2 -1 ]
-        medioDer = listaOrdenada[longitudLista // 2 ]
-        mediana = (medioIzq + medioDer) / 2
-    else:
-        mediana = listaOrdenada [longitudLista // 2]
-        return mediana
+#     if longitudLista % 2 == 0:
+#         medioIzq = listaOrdenada[longitudLista // 2 -1 ]
+#         medioDer = listaOrdenada[longitudLista // 2 ]
+#         mediana = (medioIzq + medioDer) / 2
+#     else:
+#         mediana = listaOrdenada [longitudLista // 2]
+#         return mediana
 
-def CALCULAR_CUARTILES(lista):
-    listaOrdenada=sorted(lista)
-    longitudLista=len(lista)
+# def CALCULAR_CUARTILES(lista):
+#     listaOrdenada=sorted(lista)
+#     longitudLista=len(lista)
 
-    q1_index= int(0.25 * (longitudLista + 1))
-    q2_index= int(0.5 * (longitudLista + 1))
-    q3_index= int(0.75 * (longitudLista + 1))
+#     q1_index= int(0.25 * (longitudLista + 1))
+#     q2_index= int(0.5 * (longitudLista + 1))
+#     q3_index= int(0.75 * (longitudLista + 1))
 
-    q1= listaOrdenada[q1_index]
-    q2= listaOrdenada[q2_index]
-    q3= listaOrdenada[q3_index]
+#     q1= listaOrdenada[q1_index]
+#     q2= listaOrdenada[q2_index]
+#     q3= listaOrdenada[q3_index]
 
-    return q1, q2, q3 
+#     return q1, q2, q3 
 
-def RANGO(n_mayor, n_menor):
-    # en la función rango, se resta el menor valor de las muestras al mayor valor de la muestra
-    valor_rango = n_mayor - n_menor
-    return valor_rango
+# def RANGO(n_mayor, n_menor):
+#     # en la función rango, se resta el menor valor de las muestras al mayor valor de la muestra
+#     valor_rango = n_mayor - n_menor
+#     return valor_rango
+
+numero_muestra = 0
+lista_muestras = []
+print("Ingrese las muestras una por una. Cuando ya no desee agregar más, coloque la palabra FIN")
+while True:
+    # Se ingresan los numeros uno por uno y mediante el "numero_muestra + 1" se va incrementando en la terminal el numero que se ingresa.
+    valor = input(f"Ingrese número de muestra {numero_muestra + 1}: ")
+    # Aca se evalua si el elemento ingresado es de valor numerico. 
+    # Si es un valor numerico, se lo agrega a la lista "lista_muestras".
+    # Si el valor es NO numerico, se evalua; si es la palabra "FIN" no se desean agregar mas números a la lista, si es una letra random marca no valido y deja volver a intentar.
+    if valor.isdigit():
+        lista_muestras.append(valor)
+        numero_muestra += 1
+    elif valor.isalpha():
+        if valor.upper() == "FIN":
+            print("Fin de las muestras")
+            break
+        else:
+            print("Comando no válido, intente de nuevo.")
+lista_muestras = sorted(lista_muestras)
+print(lista_muestras)
 
 while True:
-    numero_muestra = 0
-    lista_muestras = []
-    print("Ingrese las muestras una por una. Cuando ya no desee agregar más, coloque la palabra FIN")
-    while True:
-        # Se ingresan los numeros uno por uno y mediante el "numero_muestra + 1" se va incrementando en la terminal el numero que se ingresa.
-        valor = input(f"Ingrese número de muestra {numero_muestra + 1}: ")
-        # Aca se evalua si el elemento ingresado es de valor numerico. 
-        # Si es un valor numerico, se lo agrega a la lista "lista_muestras".
-        # Si el valor es NO numerico, se evalua; si es la palabra "FIN" no se desean agregar mas números a la lista, si es una letra random marca no valido y deja volver a intentar.
-        # CATY, tratar de hacer las lineas complejas en funciones reutilizables. 
-        if valor.isdigit():
-            lista_muestras.append(valor)
-            numero_muestra += 1
-        elif valor.isalpha():
-            if valor.upper() == "FIN":
-                print("Fin de las muestras")
-                break
-            else:
-                print("Comando no válido, intente de nuevo.")
-    print(lista_muestras)
-    
+    cambios = input("Desea realizar algun cambio? \n 1 = SI | 2 = NO: ")
+    if cambios == 1:
+        si = input("Que cambio desea realizar? 1 = eliminar | 2 = modificar | 3")
+
+
+
+while True:    
     #Este print le permite al usuario seleccionar que tipo de informacion desea recibir de la lista de muestras
     # CATY, hay que hacer que esto sea mas util para el usuario (agregar numeros olvidados, que no se termine de una, etc) 
     print("¿Qué desea conocer sobre esta lista?\n 1. MEDIA ARITMÉRICA.\n 2. MODA.\n 3. MEDIANA. \n 4. MÁXIMA.\n 5. MÍNIMA.\n 6. RANGO.\n 7. CUARTILES.\n 8. DESVIO ESTANDAR")
