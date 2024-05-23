c861be96365987839a8d55f4e87dbc2b99dfe06b
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-22 21:25:26 -0300

    Update Funciones.py
    Se reparó la función de "intervalos" que no funcionaba correctamente

--- Funciones.py
+++ Funciones.py
@@ -154,24 +154,24 @@ def CALCULAR_AMPLITUD_INTERVALOS(datos, numero_intervalos):
     Maximo = max(datos)  # Encontrar el valor máximo en los datos
     Amplitud = (Maximo - Minimo) / numero_intervalos  # Calcular la amplitud del intervalo
     return Amplitud
-
+    
 def CALCULAR_INTERVALOS_CLASE(datos):
-    import math # Se utiliza la función math para luego sacar el logaritmo 2 lineas más abajo
+    import math
+    
     Cantidad_Datos = len(datos)
-    Numero_Intervalos = (1+ 3.322) * math.log(Cantidad_Datos) # Determinar el número de intervalos utilizando la fórmula de Sturges
-    Amplitud = CALCULAR_AMPLITUD_INTERVALOS(datos, Numero_Intervalos)  # Calcular la amplitud del intervalo
-
+    Numero_Intervalos = 1 + 3.322 * math.log10(Cantidad_Datos) #se utiliza la formula de Sturges para usar la función de logaritmo
+    Amplitud = CALCULAR_AMPLITUD_INTERVALOS(datos, Numero_Intervalos) #Se llama la función "CALCULAR_AMPLITUD_INTERVALOS" que creamos antes
+    
     Limite_Inferior = min(datos)
     Limite_Superior = Limite_Inferior + Amplitud
-    Intervalos = []
+    Intervalos = [] #se crea la variable intervalos
 
-    while Limite_Superior <= max(datos):
-        Intervalos.append((Limite_Inferior, Limite_Superior)) # Se unen los límites para crear el intervalo
+    while Limite_Superior <= max(datos): 
+        Intervalos.append((Limite_Inferior, Limite_Superior)) #se crea el intervalo uniendo los dos límites
         Limite_Inferior = Limite_Superior
         Limite_Superior += Amplitud
-
-    return Intervalos 
-
+    return Intervalos
+    
 # Funciones del input
 
 # Converti el input en una funcion, para que si en algun momento se desean agregar mas elementos, se pueda reutilizar
