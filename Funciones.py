437d49dc9b52108a2d450fd3f07a1f30034f3072
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-05-25 19:01:18 -0300

    Update Funciones.py
    cambio de palabra muestras a datos en print en AGREGAR_ELEMENTOS_INPUT

--- Funciones.py
+++ Funciones.py
@@ -190,10 +190,10 @@ def CALCULAR_INTERVALOS_CLASE(datos):
 # Converti el input en una funcion, para que si en algun momento se desean agregar mas elementos, se pueda reutilizar
 def AGREGAR_ELEMENTOS_INPUT(lista):
     numero_muestra = 0
-    print("Ingrese las muestras una por una. Cuando ya no desee agregar más, coloque la palabra FIN")
+    print("Ingrese los datos uno por uno (Presione enter para pasar a la siguiente). Cuando ya no desee agregar más, coloque la palabra FIN")
     while True:
         # Se ingresan los numeros uno por uno y mediante el "numero_muestra + 1" se va incrementando en la terminal el numero que se ingresa.
-        valor = input(f"Ingrese número de muestra {numero_muestra + 1}: ")
+        valor = input(f"Ingrese el número de datos {numero_muestra + 1}: ")
         # Aca se evalua si el elemento ingresado es de valor numerico. 
         # Si es un valor numerico, se lo agrega a la lista "lista_muestras".
         # Si el valor es NO numerico, se evalua; si es la palabra "FIN" no se desean agregar mas números a la lista, si es una letra random marca no valido y deja volver a intentar.
@@ -202,7 +202,7 @@ def AGREGAR_ELEMENTOS_INPUT(lista):
             numero_muestra += 1
         elif valor.isalpha():
             if valor.upper() == "FIN":
-                print("Fin de las muestras")
+                print("Fin de la muestra")
                 break
             else:
                 print("Comando no válido, intente de nuevo.")
